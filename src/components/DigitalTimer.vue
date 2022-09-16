571c834b580b96d50bb92e3c70a22b1deafca2e5
fhcprom <fhcprogamer999@gmail.com>
2022-09-16 12:59:50 +0800

    added windmill and doors

--- src/components/DigitalTimer.vue
+++ src/components/DigitalTimer.vue
@@ -1,41 +1,349 @@
 <template>
   <div class="digital-timer-container">
-    <div class="digital-timer mt-4">
-      <p class="digital-timer-word">00</p>
-      <p class="digital-timer-word">:</p>
-      <p class="digital-timer-word">23</p>
-      <p class="digital-timer-word">:</p>
-      <p class="digital-timer-word">12</p>
-      <p class="digital-timer-label">hour</p>
-      <p class="digital-timer-label"></p>
-      <p class="digital-timer-label">minute</p>
-      <p class="digital-timer-label"></p>
-      <p class="digital-timer-label">second</p>
+    <div class="digital-timer-doors">
+      <svg
+        viewBox="0 0 95.063232 58.56778"
+        version="1.1"
+        id="svg5"
+        xml:space="preserve"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:svg="http://www.w3.org/2000/svg"
+      >
+        <defs id="defs2">
+          <linearGradient id="linearGradient5448">
+            <stop
+              style="stop-color: #28ede2; stop-opacity: 1"
+              offset="0"
+              id="stop5444"
+            />
+            <stop
+              style="stop-color: #053432; stop-opacity: 1"
+              offset="1"
+              id="stop5446"
+            />
+          </linearGradient>
+          <linearGradient id="linearGradient5139">
+            <stop
+              style="stop-color: #07645f; stop-opacity: 1"
+              offset="0"
+              id="stop5135"
+            />
+            <stop
+              style="stop-color: #053432; stop-opacity: 1"
+              offset="1"
+              id="stop5137"
+            />
+          </linearGradient>
+          <linearGradient
+            xlink:href="#linearGradient5139"
+            id="linearGradient5141"
+            x1="113.68681"
+            y1="66.997261"
+            x2="113.50549"
+            y2="45.057693"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="matrix(1,0,0,1.0125152,-21.502016,-41.239792)"
+          />
+          <radialGradient
+            xlink:href="#linearGradient5139"
+            id="radialGradient5424"
+            cx="81.716286"
+            cy="45.042179"
+            fx="81.716286"
+            fy="45.042179"
+            r="47.52816"
+            gradientTransform="matrix(0.96930889,-0.00238818,4.4421898e-4,0.18030078,-24.803146,-4.820926)"
+            gradientUnits="userSpaceOnUse"
+          />
+          <linearGradient
+            xlink:href="#linearGradient5139"
+            id="linearGradient5432"
+            x1="24.931318"
+            y1="49.5"
+            x2="24.387362"
+            y2="65.456047"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="translate(-21.502016,-40.72873)"
+          />
+          <radialGradient
+            xlink:href="#linearGradient5448"
+            id="radialGradient5442"
+            cx="68.744423"
+            cy="74.760246"
+            fx="68.744423"
+            fy="74.760246"
+            r="41.044861"
+            gradientTransform="matrix(1.534002,0.00125559,-5.4172533e-4,0.66186893,-58.188029,-21.071911)"
+            gradientUnits="userSpaceOnUse"
+          />
+          <radialGradient
+            xlink:href="#linearGradient5448"
+            id="radialGradient5442-7"
+            cx="68.744423"
+            cy="74.760246"
+            fx="68.744423"
+            fy="74.760246"
+            r="41.044861"
+            gradientTransform="matrix(-1.5339962,-0.00441725,0.00190587,-0.66186641,153.20747,79.759918)"
+            gradientUnits="userSpaceOnUse"
+          />
+          <radialGradient
+            xlink:href="#linearGradient5139"
+            id="radialGradient5424-8"
+            cx="81.716286"
+            cy="45.042179"
+            fx="81.716286"
+            fy="45.042179"
+            r="47.52816"
+            gradientTransform="matrix(-0.96931175,3.9037323e-4,-7.2607698e-5,-0.18030131,119.85614,63.440168)"
+            gradientUnits="userSpaceOnUse"
+          />
+          <linearGradient
+            xlink:href="#linearGradient5139"
+            id="linearGradient7001"
+            gradientUnits="userSpaceOnUse"
+            x1="24.508106"
+            y1="64.097054"
+            x2="24.708891"
+            y2="45.510288"
+            gradientTransform="rotate(-179.88191,58.291693,49.610526)"
+          />
+          <linearGradient
+            xlink:href="#linearGradient5139"
+            id="linearGradient7003"
+            gradientUnits="userSpaceOnUse"
+            gradientTransform="matrix(-0.99516516,-0.00205786,0.00207676,-1.0109404,115.9511,99.74238)"
+            x1="113.29413"
+            y1="50.417038"
+            x2="113.36061"
+            y2="67.787567"
+          />
+        </defs>
+        <g id="doors">
+          <g id="upper-door" ref="upperDoor">
+            <path
+              style="
+                fill: url(#radialGradient5442);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.4874;
+                stroke-dasharray: none;
+              "
+              d="m 6.762338,5.939439 h 81.817596 l -0.15866,23.216022 -81.578476,0.07577 z"
+              id="path1154"
+            />
+            <path
+              style="
+                fill: url(#linearGradient5432);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.49902;
+                stroke-dasharray: none;
+              "
+              d="M 0.169172,0 6.779184,5.908419 6.864824,29.226761 0,29.306891 Z"
+              id="path4437"
+            />
+            <path
+              style="
+                fill: url(#radialGradient5424);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.499999;
+                stroke-dasharray: none;
+              "
+              d="M 0.17415,0.001924 6.806363,5.96083 88.535754,5.9155 95.063239,0.022642 Z"
+              id="path5069"
+            />
+            <path
+              style="
+                fill: url(#linearGradient5141);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.503118;
+                stroke-dasharray: none;
+              "
+              d="m 88.503594,5.948418 6.410585,-5.841733 -0.0304,29.066498 -6.466925,-0.02266 z"
+              id="path5073"
+            />
+            <path
+              style="
+                fill: #000000;
+                stroke: #ffffff;
+                stroke-width: 0.2;
+                stroke-dasharray: none;
+              "
+              d="M 94.812006,29.106563 0.00749356,29.12259"
+              id="path1160"
+            />
+          </g>
+          <g id="lower-door" ref="lowerDoor">
+            <path
+              style="
+                fill: #ffffff;
+                stroke: #ffffff;
+                stroke-width: 0.2;
+                stroke-dasharray: none;
+              "
+              d="M 94.852775,29.310822 0.06045923,29.30281"
+              id="path1160-2"
+            />
+            <path
+              style="
+                fill: url(#radialGradient5442-7);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.4874;
+                stroke-dasharray: none;
+              "
+              d="M 88.72087,52.614763 6.495481,52.446132 6.70199,29.230487 l 81.98643,0.09237 z"
+              id="path1154-5"
+            />
+            <path
+              style="
+                fill: url(#linearGradient7001);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.49902;
+                stroke-dasharray: none;
+              "
+              d="m 94.893815,58.567778 -6.507163,-5.92203 0.07575,-23.409128 6.404576,0.02468 z"
+              id="path4437-4"
+            />
+            <path
+              style="
+                fill: url(#radialGradient5424-8);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.499999;
+                stroke-dasharray: none;
+              "
+              d="M 94.934175,58.565844 88.67689,52.593282 6.9362462,52.390835 0,58.349554 Z"
+              id="path5069-4"
+            />
+            <path
+              style="
+                fill: url(#linearGradient7003);
+                fill-opacity: 1;
+                stroke: none;
+                stroke-width: 0.501511;
+                stroke-dasharray: none;
+              "
+              d="M 6.9458102,52.401183 0.04700525,58.316648 0.04656271,29.199445 6.9217243,29.224013 Z"
+              id="path5073-5"
+            />
+          </g>
+        </g>
+      </svg>
     </div>
-    <rectangle-button
-      digitalTimer="true"
-      width="100%"
-      padding="10px 25px"
-      label="Break Time!"
-      class="mt-2 text-xl"
-    ></rectangle-button>
+
+    <div v-if="timerStarted" class="w-full h-full">
+      <div class="digital-timer mt-4">
+        <p class="digital-timer-word">00</p>
+        <p class="digital-timer-word">:</p>
+        <p class="digital-timer-word">23</p>
+        <p class="digital-timer-word">:</p>
+        <p class="digital-timer-word">12</p>
+        <p class="digital-timer-label">hour</p>
+        <p class="digital-timer-label"></p>
+        <p class="digital-timer-label">minute</p>
+        <p class="digital-timer-label"></p>
+        <p class="digital-timer-label">second</p>
+      </div>
+      <rectangle-button
+        :digitalTimer="true"
+        :containSVG="false"
+        label="Gimme a break!"
+        class="mt-2 text-lg"
+      ></rectangle-button>
+    </div>
+
+    <div class="timer-task-creator">
+      <rectangle-button
+        :digitalTimer2="true"
+        :containSVG="false"
+        label="Create New Task"
+      ></rectangle-button>
+    </div>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref } from "@vue/reactivity";
+import { onMounted } from "@vue/runtime-core";
 import RectangleButton from "./buttons/RectangleButton.vue";
-export default {
-  components: { RectangleButton },
-};
+
+const timerStarted = ref(false);
+const upperDoor = ref(null);
+const lowerDoor = ref(null);
+const upperDoorY = ref(0);
+const lowerDoorY = ref(0);
+
+function openDoors() {}
+
+onMounted(() => {
+  upperDoor.value.style.transform = "translate(0px,0px)";
+  console.log(upperDoor.value.style);
+  // upperDoor.value.classList.add("upper-door-selected");
+  // lowerDoor.value.classList.add("lower-door-selected");
+});
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .digital-timer-container {
   width: 400px;
   height: 250px;
+  position: relative;
   background: transparent;
   border: 1px solid white;
 }
+
+.digital-timer-doors {
+  @include transition-ease(1.5s);
+
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 10;
+
+  .upper-door-selected {
+    animation: slideUp 3s linear forwards;
+  }
+  .lower-door-selected {
+    animation: slideDown 3s linear forwards;
+  }
+
+  // &:hover .upper-door {
+  //   animation: slideUp 1.5s linear forwards;
+  // }
+  // &:hover .lower-door {
+  //   animation: slideDown 1.5s linear forwards;
+  // }
+}
+
+.timer-task-creator {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  align-items: center;
+  justify-items: center;
+}
+
+@keyframes slideUp {
+  100% {
+    transform: translateY(-110px);
+  }
+}
+
+@keyframes slideDown {
+  100% {
+    transform: translateY(110px);
+  }
+}
+
 .digital-timer {
   margin-top: 20px;
   display: grid;
---
+++ src/components/dialogs/DashboardDialogs.vue
@@ -0,0 +1,256 @@
+<template>
+  <Dialog :open="isOpen" @close="setIsOpen" class="relative z-50">
+    <div class="fixed inset-0 flex items-center justify-center p-4">
+      <!--
+        .............................
+        .............................
+         Dialog for creating new task 
+         .............................
+         .............................-->
+      <DialogPanel
+        v-if="taskDialog == true"
+        :class="classes"
+        class="w-full rounded bg-white"
+      >
+        <div class="task-dialog-image"></div>
+
+        <DialogDescription class="task-dialog-content z-20">
+          <div class="task-dialog-table-layout">
+            <div class="goal-label">Goal</div>
+            <div>Make a design draf for Art of Breaking</div>
+
+            <div class="task-label">New Task</div>
+            <textarea class="task-input" rows="3"></textarea>
+          </div>
+          <div class="task-dialog-btn">
+            <rectangle-button
+              label="cancel"
+              :cancel="true"
+              @click="setIsOpen(false)"
+            ></rectangle-button>
+
+            <rectangle-button
+              label="Add"
+              :modal="true"
+              @click="setIsOpen(false)"
+            ></rectangle-button>
+          </div>
+        </DialogDescription>
+
+        <div class="task-dialog-accessories z-10"></div>
+      </DialogPanel>
+
+      <!--
+        .............................
+        .............................
+         Dialog for creating new goal
+         .............................
+         .............................-->
+
+      <DialogPanel
+        v-if="goalDialog == true"
+        :class="classes"
+        class="w-full rounded bg-white"
+      >
+        <div class="goal-dialog-image"></div>
+
+        <DialogDescription class="goal-dialog-content z-20">
+          <div class="goal-dialog-table-layout">
+            <div class="text-2xl">Your One and Only Goal</div>
+            <div>...for now</div>
+            <input class="goal-input" type="text" placeholder="goal" />
+          </div>
+          <div class="task-dialog-btn">
+            <rectangle-button
+              class="text-xs"
+              label="cancel"
+              :cancel="true"
+              @click="setIsOpen(false)"
+            ></rectangle-button>
+
+            <rectangle-button
+              label="Confirm"
+              :modal="true"
+              @click="setIsOpen(false)"
+            ></rectangle-button>
+          </div>
+        </DialogDescription>
+
+        <div class="goal-dialog-accessories z-10"></div>
+      </DialogPanel>
+    </div>
+  </Dialog>
+</template>
+
+<script setup>
+import RectangleButton from "@/components/buttons/RectangleButton.vue";
+import { onUnmounted, ref, computed } from "vue";
+import {
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+  DialogDescription,
+} from "@headlessui/vue";
+
+const props = defineProps({
+  taskDialog: {
+    type: Boolean,
+  },
+  goalDialog: {
+    type: Boolean,
+  },
+});
+
+const classes = computed(() => ({
+  "task-dialog": props.taskDialog,
+  "goal-dialog": props.goalDialog,
+}));
+
+const isOpen = ref(false);
+function setIsOpen(value) {
+  isOpen.value = value;
+}
+
+defineExpose({
+  isOpen,
+});
+</script>
+
+<style lang="scss" scoped>
+.task-dialog {
+  display: grid;
+  align-items: center;
+  max-width: 50rem;
+  padding: 1.2rem;
+  position: relative;
+
+  &-accessories {
+    position: absolute;
+    margin: 1rem;
+    border: 1px solid black;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+  }
+
+  &-image {
+    grid-column: 1/2;
+    min-width: 10rem;
+    min-height: 10rem;
+    height: 100%;
+
+    background: url("../../assets/Modals/papers.jpg");
+  }
+
+  &-content {
+    grid-column: 2/3;
+    padding: 0 1rem 0 1rem;
+    margin: 2rem 0 2rem 0;
+  }
+  &-btn {
+    width: 70%;
+    float: right;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    padding: 1rem;
+    column-gap: 1rem;
+  }
+}
+
+.goal-dialog {
+  display: grid;
+  align-items: center;
+  max-width: 50rem;
+  padding: 1.2rem;
+  position: relative;
+  grid-template-columns: 1fr 1fr;
+
+  &-accessories {
+    position: absolute;
+    margin: 1rem;
+    border: 1px solid black;
+    top: 0px;
+    right: 0px;
+    bottom: 0px;
+    left: 0px;
+  }
+
+  &-image {
+    grid-column: 1/2;
+    min-width: 100%;
+    height: 100%;
+    background: url("../../assets/Modals/goal.png");
+    background-repeat: no-repeat;
+    background-position-x: 50%;
+  }
+
+  &-content {
+    grid-column: 2/3;
+    padding: 0 1rem 0 1rem;
+    margin: 2rem 0 2rem 0;
+  }
+
+  &-btn {
+    width: 70%;
+    float: right;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    padding: 1rem;
+    column-gap: 1rem;
+  }
+}
+
+.task-dialog-table-layout {
+  display: grid;
+  grid-template-columns: 6rem 1fr;
+  padding: 1rem;
+  gap: 1rem;
+
+  .goal-label {
+    text-align: center;
+    height: 1.5rem;
+    color: white;
+    background: rgb(251, 146, 60);
+  }
+  .task-label {
+    text-align: center;
+    height: 1.5rem;
+    color: white;
+    background: rgb(234, 179, 8);
+  }
+
+  .task-input {
+    width: 100%;
+    padding: 0.5rem;
+    min-width: 10rem;
+    box-shadow: 2px 2px 8px rgba(black, 0.3);
+  }
+
+  .task-input:focus {
+    border: none;
+    outline: none;
+  }
+}
+
+.goal-dialog-table-layout {
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  padding: 1rem;
+  gap: 1rem;
+  text-align: center;
+  .goal-input {
+    border-bottom: 2px solid gray;
+    width: 100%;
+  }
+
+  .goal-input:focus {
+    border-bottom: 2px solid black;
+    outline: none;
+  }
+}
+</style>
