c3e70e1297d7273fc6d25d05869c3ee21de828f9
fhcprom <fhcprogamer999@gmail.com>
2023-03-08 10:22:34 +0800

    added third section

--- src/views/PageOfficial.vue
+++ src/views/PageOfficial.vue
@@ -64,7 +64,59 @@
                   <div class="time-column">2 hour 25 minutes</div>
                 </td>
               </tr>
-
+              <tr class="table-row">
+                <td class="image-column">
+                  <div class="image-column-layout">
+                    <div class="image-frame">
+                      <img
+                        class="image-adjustment"
+                        src="@/assets/officialPage/section2-3.jpg"
+                      />
+                    </div>
+                    <div class="image-details">Lily</div>
+                  </div>
+                </td>
+                <td class="details-layout">
+                  <div class="detail-column">Lorem, ipsum dolor.</div>
+                  <div class="time-column">15 minutes</div>
+                </td>
+              </tr>
+              <tr class="table-row">
+                <td class="image-column">
+                  <div class="image-column-layout">
+                    <div class="image-frame">
+                      <img
+                        class="image-adjustment"
+                        src="@/assets/officialPage/section2-2.jpg"
+                      />
+                    </div>
+                    <div class="image-details">Ashley</div>
+                  </div>
+                </td>
+                <td class="details-layout">
+                  <div class="detail-column">Lorem ipsum dolor sit.</div>
+                  <div class="time-column">1 hour 5 minutes</div>
+                </td>
+              </tr>
+              <tr class="table-row">
+                <td class="image-column">
+                  <div class="image-column-layout">
+                    <div class="image-frame">
+                      <img
+                        class="image-adjustment"
+                        src="@/assets/officialPage/section2-1.jpg"
+                      />
+                    </div>
+                    <div class="image-details">John</div>
+                  </div>
+                </td>
+                <td class="details-layout">
+                  <div class="detail-column">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing.
+                  </div>
+                  <div class="time-column">2 hour 25 minutes</div>
+                </td>
+              </tr>
               <tr class="table-row">
                 <td class="image-column">
                   <div class="image-column-layout">
@@ -84,6 +136,88 @@
               </tr>
             </tbody>
           </table>
+        </div>
+      </div>
+    </section>
+
+    <section class="third-section">
+      <div class="third-section-layout">
+        <div class="third-section-layout-2">
+          <div class="third-section-layout-3">
+            <div class="third-section-cards">
+              <div class="third-section-left-card">
+                <div class="third-section-left-card-first-line">
+                  Lorem ipsum dolor sit amet consectetur adipisicing.
+                </div>
+                <div class="third-section-left-card-second-line">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Soluta praesentium aspernatur sunt, amet illum fugit.
+                </div>
+                <div>
+                  <button class="third-section-left-card-button">
+                    <span class="third-section-left-card-button-span"
+                      >Start Planning</span
+                    >
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div class="third-section-cards">
+              <div class="third-section-right-card">
+                <div class="third-section-right-card-layout">
+                  <div class="third-section-right-card-layout-2">
+                    <div class="third-section-right-card-layout-image-section">
+                      <img
+                        class="dcBlBt"
+                        src="@/assets/officialPage/section3-1.png"
+                      />
+                      <span class="hCTbnf">Lorem, ipsum.</span>
+                    </div>
+                    <div class="bmCMRG">
+                      <div class="eEfpch">Lorem, ipsum dolor.</div>
+                    </div>
+                  </div>
+                  <div class="third-section-right-card-layout-2">
+                    <div class="third-section-right-card-layout-image-section">
+                      <img
+                        class="dcBlBt"
+                        src="@/assets/officialPage/section3-1.png"
+                      />
+                      <span class="hCTbnf">Lorem, ipsum.</span>
+                    </div>
+                    <div class="bmCMRG">
+                      <div class="eEfpch">Lorem, ipsum dolor.</div>
+                    </div>
+                  </div>
+                  <div class="third-section-right-card-layout-2">
+                    <div class="third-section-right-card-layout-image-section">
+                      <img
+                        class="dcBlBt"
+                        src="@/assets/officialPage/section3-1.png"
+                      />
+                      <span class="hCTbnf">Lorem, ipsum.</span>
+                    </div>
+                    <div class="bmCMRG">
+                      <div class="eEfpch">Lorem, ipsum dolor.</div>
+                    </div>
+                  </div>
+                  <div class="third-section-right-card-layout-2">
+                    <div class="third-section-right-card-layout-image-section">
+                      <img
+                        class="dcBlBt"
+                        src="@/assets/officialPage/section3-1.png"
+                      />
+                      <span class="hCTbnf">Lorem, ipsum.</span>
+                    </div>
+                    <div class="bmCMRG">
+                      <div class="eEfpch">Lorem, ipsum dolor.</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -192,10 +326,9 @@ let taskIsRunning = ref(false);
 
 .second-section-content {
   width: 100%;
-  border-right: 0px;
-  border-left: 0px;
   margin-top: -1px;
   overflow-x: auto;
+  border-bottom: 1px solid var(--gray-300);
 }
 
 .table-layout {
@@ -262,6 +395,144 @@ let taskIsRunning = ref(false);
   color: var(--teal-800);
 }
 
+.third-section-layout {
+  display: flex;
+  flex-shrink: 0;
+  width: 100%;
+  max-width: 1180px;
+  margin: 0px auto;
+  padding: 24px;
+}
+
+.third-section-layout-2 {
+  display: flex;
+  flex-direction: row;
+  flex: 1 1 50%;
+}
+
+.third-section-layout-3 {
+  flex-direction: column;
+  height: auto;
+  overflow: unset;
+  display: flex;
+  flex: 1 0 auto;
+  position: relative;
+  background-color: rgb(var(--gray0));
+  width: 100%;
+}
+
+.third-section-cards {
+  display: flex;
+  flex: 1 1 50%;
+  justify-content: center;
+  flex-direction: row;
+  width: 100%;
+}
+
+.third-section-left-card {
+  display: flex;
+  flex-direction: column;
+  max-width: 325px;
+  margin-left: 0px;
+  padding: 0px 16px;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  z-index: 1;
+  min-width: 250px;
+}
+
+.third-section-left-card-first-line {
+  line-height: 1.3;
+  color: var(--gray-900);
+  font-size: 32px;
+}
+
+.third-section-left-card-second-line {
+  font-size: 14px;
+  line-height: 1.6;
+  color: var(--gray-900);
+  margin: 0px 0px 16px;
+}
+
+.third-section-left-card-button {
+  width: fit-content;
+  margin-bottom: 1rem;
+  position: relative;
+  border-radius: 4px;
+  color: var(--gray-100);
+  cursor: pointer;
+  transition: all 80ms ease-in-out 0s;
+  padding: 8px 16px;
+  font-size: 14px;
+  border: 1px solid var(--yellow-500);
+  background-color: var(--yellow-500);
+}
+
+.third-section-left-card-button-span {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  pointer-events: none;
+  flex-wrap: nowrap;
+  white-space: nowrap;
+}
+
+.third-section-right-card {
+  width: 100%;
+}
+
+.third-section-right-card-layout {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.third-section-right-card-layout-2 {
+  padding: 16px;
+  border: 1px solid var(--gray-300);
+  border-radius: 0px;
+  margin-bottom: 0px;
+  margin-top: -1px;
+  display: flex;
+  width: 100%;
+  white-space: nowrap;
+  cursor: pointer;
+  opacity: 0.9;
+}
+
+.third-section-right-card-layout-image-section {
+  justify-content: start;
+  display: flex;
+  align-items: center;
+}
+
+.dcBlBt {
+  height: 38px;
+  width: 38px;
+  margin-right: 15px;
+}
+.hCTbnf {
+  width: min-content;
+  white-space: normal;
+  line-height: 1.3;
+  color: rgb(var(--gray100));
+  font-size: 18px;
+
+  display: block;
+  margin: 0px;
+}
+.bmCMRG {
+  margin-left: auto;
+  justify-content: start;
+  display: flex;
+  align-items: center;
+}
+.eEfpch {
+  margin-left: auto;
+}
+
 @media screen and (min-width: $breakpoint-small) {
   .first-section-words {
     padding-top: 100px;
@@ -291,6 +562,27 @@ let taskIsRunning = ref(false);
   }
   .time-column {
     width: 200px;
+  }
+
+  .third-section-layout-3 {
+    flex-direction: row;
+  }
+
+  .third-section-cards {
+    justify-content: unset;
+  }
+
+  .third-section-left-card {
+    align-items: unset;
+    text-align: unset;
+  }
+
+  .third-section-right-card-layout-2 {
+    border: none;
+  }
+
+  .third-section-right-card-layout-2:hover {
+    box-shadow: rgb(0 0 0 / 12%) 0px 8px 24px;
   }
 }
 </style>
